c836ab517a6ad8bb39813cafea6ed6d335df8671
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-10 21:41:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "9";
+var prefix = "13";
 
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
@@ -8,8 +8,8 @@ client.on('ready', () => {
 
 
 client.on('ready', async() => {
-var server = "510887418874953737"; // ايدي السررفر
-var channel = "510887418874953739";//ايدي الروم
+var server = "510887760228515846"; // ايدي السررفر
+var channel = "510887760228515849";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast, The_Ghast, The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , ')
     },305);
@@ -31,4 +31,4 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
   }
  });
 
-client.login('NTEwODk3NjkwNTQ0OTYzNTk4.DsjCLA.SzD6t8kl_cH8BDplJBExc65JsCY');
+client.login('NTEwOTE0MTU1Nzk3MTUxNzU2.DsjTaQ.MSE7rjPuoQVja89L6fk0UZTiAo4');
